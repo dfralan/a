02c44300bf3f9d93a9605d4bc43e693b17b0cae0
dfralan <dfralan@gmail.com>
2025-07-21 18:18:36 -0300

    commit

---
+++ a/Sources/Views/Followers/FollowersView.swift
@@ -0,0 +1,51 @@
+// a
+
+import SwiftUI
+import RealmSwift
+import SDWebImageSwiftUI
+
+struct FollowersView: View {
+    
+    @EnvironmentObject var nostrData: NostrData
+    @ObservedRealmObject var userProfile: RUserProfile
+    @ObservedResults(RContactList.self) var contactLists
+
+    var followedBy: [RUserProfile] {
+        if let followedBy = contactLists.filter("publicKey = %@", userProfile.publicKey).first?.followedBy {
+            return Array(followedBy.sorted(byKeyPath: "name", ascending: false))
+        }
+        return  []
+    }
+
+    var body: some View {
+        List {
+
+            Section("Followers") {
+                
+                ForEach(followedBy) { userProfile in
+                    
+                    NavigationLink(value: Navigation.NavUserProfile(userProfile: userProfile)) {
+                        UserProfileListViewRow(userProfile: userProfile)
+                    }
+                    .id(userProfile.publicKey)
+                }
+                
+            }
+
+        }
+        .listStyle(.plain)
+        .navigationTitle("")
+        .toolbar {
+            ToolbarItem(placement: .principal) {
+                UserProfileNavigationTitle(userProfile: userProfile)
+            }
+        }
+    }
+}
+
+struct FollowersView_Previews: PreviewProvider {
+    static var previews: some View {
+        FollowersView(userProfile: RUserProfile.createEmpty(withPublicKey: "abc"))
+            .environmentObject(NostrData.shared)
+    }
+}
